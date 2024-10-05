659a21ca2fc890ae1eacc27a95172da5b6b37a92
SundarakrishnanN <sundar050804@gmail.com>
2024-10-05 19:47:12 +0530

    testing vercel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <div className="min-h-screen bg-gray-50 relative">
       {/* Background Video */}
       <video
-        className="absolute inset-0 w-full h-full object-cover filter"
+        className="absolute inset-0 w-full h-full object-cover filter contrast-140"
         autoPlay
         loop
         muted
@@ -22,7 +22,7 @@ export default function Home() {
       {/* Hero Section */}
       <header className="flex flex-col items-center justify-center h-screen text-center relative z-10 ">
       <h1 className="text-8xl font-bold text-white">Astra Robotics</h1>
-      <p className="mt-4 text-lg text-white justify-left w-[1000px]">
+      <p className="mt-4 text-lg text-white justify-left w-[1000px] ">
         Astra Robotics is an interdisciplinary student-led technical team at RV College of Engineering, Bengaluru, dedicated to developing innovative solutions that address everyday societal challenges through the fields of robotics and automation. Our team is committed to driving innovation, as we firmly believe that automation and technology are the keys to creating a more efficient and sustainable future.
       </p>
       </header>
