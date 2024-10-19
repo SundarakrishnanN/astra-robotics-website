8206c5b60f9338ccdba293d8022c4deb3b77f13b
sA-Ge1 <adityakarumbaiah@gmail.com>
2024-10-19 12:17:37 +0530

    genesis page added

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,7 +6,7 @@ import { JSX, SVGProps } from "react"
 
 export default function Component() {
   return (
-    <header className="fixed top-0 left-0 z-50 w-full ">
+    <header className="fixed top-0 left-0 z-50 w-full bg-gray-500 bg-opacity-50">
       <div className="container flex h-16 items-center justify-between px-4 md:px-6">
         <Link href="#" className="flex items-center gap-2" prefetch={false}>
         <img src="/astralogo.png" alt="Logo" className="h-12 w-auto" />
