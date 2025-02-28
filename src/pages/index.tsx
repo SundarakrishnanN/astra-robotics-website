33defd3ffd53880f079d413b144c64b96ca42fcf
SundarakrishnanN <sundar050804@gmail.com>
2025-02-28 18:53:41 +0530

    mobile fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import Navbar from '@/components/navbar';
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-50 relative">
-      {/* Background Video */ }
+      {/* Background Video */}
       <video
         className="absolute inset-0 w-full h-full object-cover filter brightness-50"
         autoPlay
@@ -22,24 +22,26 @@ export default function Home() {
       <Navbar />
 
       {/* Hero Section */}
-      <header className="flex flex-col items-center justify-center h-screen text-center relative z-10 px-4">
-        <h1 className="text-8xl font-bold text-white drop-shadow-lg">
+      <header className="flex flex-col items-center justify-center h-screen text-center relative z-10 px-6">
+        <h1 className="text-4xl md:text-8xl font-bold text-white drop-shadow-lg">
           Astra Robotics
         </h1>
-        <p className="mt-4 text-lg text-gray-200 leading-relaxed w-full max-w-4xl">
-          Astra Robotics is an interdisciplinary student-led technical team at RV College of Engineering, Bengaluru, dedicated to developing innovative solutions that address everyday societal challenges through the fields of robotics and automation. Our team is committed to driving innovation, as we firmly believe that automation and technology are the keys to creating a more efficient and sustainable future.
+        <p className="mt-4 text-base md:text-lg text-gray-200 leading-relaxed max-w-lg md:max-w-4xl">
+          Astra Robotics is an interdisciplinary student-led technical team at
+          RV College of Engineering, Bengaluru, dedicated to developing
+          innovative solutions that address everyday societal challenges through
+          robotics and automation. Our team is committed to driving innovation,
+          as we firmly believe that automation and technology are the keys to
+          creating a more efficient and sustainable future.
         </p>
-        <div className="mt-8">
+        <div className="mt-6">
           <Link href="/projects">
-            <Button className="px-6 py-3 text-lg bg-blue-600 hover:bg-blue-700 text-white rounded-lg shadow-md">
+            <Button className="px-5 py-3 text-base md:text-lg bg-blue-600 hover:bg-blue-700 text-white rounded-lg shadow-md">
               Explore Projects
             </Button>
           </Link>
         </div>
       </header>
-
-      {/* Additional Content Section */}
-      
     </div>
   );
 }
