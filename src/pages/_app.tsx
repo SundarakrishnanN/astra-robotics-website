8cdab2936810d79bd1cdee291677b99f2730e564
SundarakrishnanN <sundar050804@gmail.com>
2025-03-03 17:54:58 +0530

    metadata updatE

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Navbar from "@/components/navbar";
-
+import Head from "next/head";
 export default function App({ Component, pageProps }: AppProps) {
   return (<>
 <Navbar />
+<Head>
+        <title>Astra Robotics</title>
+        <meta name="description" content="Astra Robotics is an interdisciplinary robotics club of RVCE." />
+        <meta name="keywords" content="Robotics, Student Club" />
+        <meta property="og:title" content="Astra Robotics" />
+        <meta property="og:description" content="Astra Robotics is an interdisciplinary robotics club of RVCE." />
+        <meta property="og:image" content="/astralogo.png" />
+        <link rel="icon" href="/astralogo.png" />
+      </Head>
   <Component {...pageProps} /></>);
 }
